60207418e1c2143ebb82d94f746ade70e632681e
Paul Cioanca <paul.cioanca@supabase.io>
2023-12-10 21:11:11 +0200

    feat: add the possibility to monitor multiple projects at a time

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,56 @@
 #!/bin/bash
 
-if [ -z "$SUPABASE_PROJECT_REF" ]; then
-  echo "\$SUPABASE_PROJECT_REF is not set. Exiting."
-  exit 1
-fi
+set -e
 
-if [ -z "$SUPABASE_SERVICE_ROLE_KEY" ]; then
-  echo "\$SUPABASE_SERVICE_ROLE_KEY is not set. Exiting."
-  exit 1
-fi
+if [ -z "$SUPABASE_ACCESS_TOKEN" ]; then
+  echo "Setting up single-project monitoring."
 
-sed -i "s/__SUPABASE_PROJECT_REF__/$SUPABASE_PROJECT_REF/g" /etc/prometheus/prometheus.yml
-sed -i "s/__SUPABASE_SERVICE_ROLE_KEY__/$SUPABASE_SERVICE_ROLE_KEY/g" /etc/prometheus/prometheus.yml
+  if [ -z "$SUPABASE_PROJECT_REF" ]; then
+    echo "\$SUPABASE_PROJECT_REF is not set. Exiting."
+    exit 1
+  fi
+
+  if [ -z "$SUPABASE_SERVICE_ROLE_KEY" ]; then
+    echo "\$SUPABASE_SERVICE_ROLE_KEY is not set. Exiting."
+    exit 1
+  fi
+
+  cat /etc/prometheus/prometheus.target.yml.tpl >> /etc/prometheus/prometheus.yml
+
+  sed -i "s/__SUPABASE_PROJECT_REF__/$SUPABASE_PROJECT_REF/g" /etc/prometheus/prometheus.yml
+  sed -i "s/__SUPABASE_SERVICE_ROLE_KEY__/$SUPABASE_SERVICE_ROLE_KEY/g" /etc/prometheus/prometheus.yml
+else 
+  echo "Setting up multi-project monitoring."
+
+  FILTER='.[] | .id'
+  if [ -n "$SUPABASE_ORGANIZATION_ID" ]; then
+    FILTER=".[] | select(.organization_id == \"$SUPABASE_ORGANIZATION_ID\") | .id"
+  fi
+
+  PROJECT_REFS=$(curl -sX 'GET' 'https://api.supabase.com/v1/projects' \
+    -H 'accept: application/json' \
+    -H "Authorization: Bearer $SUPABASE_ACCESS_TOKEN" | jq -r "$FILTER")
+
+  for PROJECT_REF in $PROJECT_REFS; do
+    echo "Setting up project $PROJECT_REF"
+
+    API_KEYS_RESPONSE=$(curl -sX 'GET' "https://api.supabase.com/v1/projects/$PROJECT_REF/api-keys" \
+      -H 'accept: application/json' \
+      -H "Authorization: Bearer $SUPABASE_ACCESS_TOKEN")
+
+    if [[ $API_KEYS_RESPONSE == *"Failed to fetch project service details"* ]]; then
+      echo "Error: Could not retrieve API keys for project $PROJECT_REF. Project might be paused. Skipping."
+      continue
+    fi
+
+    SERVICE_ROLE_KEY=$(echo "$API_KEYS_RESPONSE" | jq -r '.[] | select(.name == "service_role") | .api_key')
+
+    cat /etc/prometheus/prometheus.target.yml.tpl >> /etc/prometheus/prometheus.yml
+
+    sed -i "s/__SUPABASE_PROJECT_REF__/$PROJECT_REF/g" /etc/prometheus/prometheus.yml
+    sed -i "s/__SUPABASE_SERVICE_ROLE_KEY__/$SERVICE_ROLE_KEY/g" /etc/prometheus/prometheus.yml
+  done
+fi
 
 mkdir -p /data/grafana/data 
 mkdir -p /data/grafana/plugins 
---
+++ prometheus/prometheus.target.yml.tpl
@@ -0,0 +1,8 @@
+  - job_name: supabase-__SUPABASE_PROJECT_REF__
+    scheme: https
+    metrics_path: "/customer/v1/privileged/metrics"
+    basic_auth:
+      username: service_role
+      password: __SUPABASE_SERVICE_ROLE_KEY__
+    static_configs:
+      - targets: ["__SUPABASE_PROJECT_REF__.supabase.co"]
